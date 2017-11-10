ab66706706d7c7993cbfb3650740dc425038ee37
Sergiy Korzh <sergey.korzh@live.com>
2017-11-10 11:08:11 +0200

    Removed the testing product key

--- EqAspNetCoreDemo01/Startup.cs
+++ EqAspNetCoreDemo01/Startup.cs
@@ -29,8 +29,6 @@ namespace Korzh.EasyQuery.AspNetCore.Demo01
             services.AddSession();
 
             services.AddEasyQuery();
-
-            Korzh.EasyQuery.License.Key = "R4QZ2YQ1v0ReQcZrTRS5zq51rZLwymOK1OmezhorFFU=";
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
