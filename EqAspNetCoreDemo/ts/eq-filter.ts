8151c4d0046613bd47cdd20b9f3883680e4906d1
Sergiy Korzh <sergey.korzh@live.com>
2019-04-16 17:04:59 +0300

    Add OrderController and view

--- EqAspNetCoreDemo/ts/eq-filter.ts
+++ EqAspNetCoreDemo/ts/eq-filter.ts
@@ -1,46 +1,4 @@
-import { EqHtmlGrid, EqViewOptions } from "@easyquery/ui"
-import { Widget } from "@easyquery/core"
-import { DataFilterViewJQuery } from '@easyquery/ui-jquery'
 
-// import '../../../packs/ui-jquery/src/jqui/jqueryui-widgets'
-// import {EqBasicView} from '../../../packs/ui-jquery/src/views/eq_basic_view'
 
-declare var $: any
-
-class PartialViewDataFilter extends DataFilterViewJQuery {
-
-    constructor() {
-        super();
-    }
-
-    protected createResultGridWidget(element: HTMLElement): Widget {
-        return new EqHtmlGrid(element);
-    }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    let options: EqViewOptions = {
-        paging: {
-            useBootstrap: true
-        },
-        context: {
-            loadModelOnStart: true,
-            clearResultOnQueryChange: false,
-            broker: {
-                serviceUrl: "/Order"
-            },
-            widgets: {
-                filterBar: {
-                    queryPanel: {
-                        attrElementFormat: "{entity} {attr}"
-                    }
-                }
-            }
-        }
-    }
-    let basicView = new PartialViewDataFilter();
-    basicView.init(options);
-    document['easyQueryView'] = basicView;
-});
 
 
