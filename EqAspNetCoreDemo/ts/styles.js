c58c73a72422b365e1c8b1ce0cae9acac9ece25e
Oleksandr Melnychenko <oleksandr.melnychenko@fts.com.ua>
2019-04-16 20:36:21 +0300

    EasyReport grid and chart

--- EqAspNetCoreDemo/ts/styles.js
+++ EqAspNetCoreDemo/ts/styles.js
@@ -1,3 +1,4 @@
 require('../node_modules/@easyquery/ui-jquery/dist/assets/css/easyquery.css');
 require('../node_modules/@easyquery/ui-jquery/dist/assets/css/eq-icons-default.css');
 require('../node_modules/@easyquery/ui-jquery/dist/assets/css/easychart.css');
+require('../node_modules/@easyquery/ui-jquery/dist/assets/css/eqview.css');
