18fd7790ba1291bb24f26f709efd0244ae27e71c
Sergiy Korzh <sergey.korzh@live.com>
2019-04-18 20:11:54 +0300

    BuildOnSync in Angular demo

--- EqAngularDemo/Startup.cs
+++ EqAngularDemo/Startup.cs
@@ -56,6 +56,7 @@ namespace EqAngularDemo
             app.UseSpaStaticFiles();
 
             app.UseEasyQuery(options => {
+                options.BuildQueryOnSync = true;
                 options.UseDbContext<AppDbContext>();
                 options.UsePaging(25);
             });
