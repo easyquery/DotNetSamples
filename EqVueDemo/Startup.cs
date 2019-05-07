7f5be086791bd94f62bbc9f8dcfbd74be4e06106
Sergiy Korzh <sergey.korzh@live.com>
2019-05-07 18:59:25 +0300

    Update page size to 30

--- EqVueDemo/Startup.cs
+++ EqVueDemo/Startup.cs
@@ -54,7 +54,7 @@ namespace EqVueDemo
             {
                 options.DefaultModelId = "NWindSQL";
                 options.UseDbConnection<System.Data.SqlClient.SqlConnection>(Configuration.GetConnectionString("EqDemoDb"));
-                options.UsePaging(20);
+                options.UsePaging(30);
             });
 
             app.UseMvc(routes =>
