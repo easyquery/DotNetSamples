cb7b020067093bd56a66e5d20859bbb5a9ecaf6d
Anton Ivanenko <anton_ivanenko@outlook.com>
2021-02-19 17:18:44 +0200

    Add separate EasyQueryPage for WPF sample

---
+++ Wpf/EqDemoWpfNet4/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+
+namespace EqDemo
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            EQB.Content = "Loading...";
+            EQB.IsEnabled = false;
+
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate {
+                NavigationService.Navigate(new Uri("EasyQueryPage.xaml", UriKind.Relative));
+            }));
+
+        }
+
+    }
+}
