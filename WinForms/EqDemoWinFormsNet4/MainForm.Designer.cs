758276ed83f66c50108b5279662065ec0f1ebd5f
Sergiy Korzh <sergey.korzh@live.com>
2021-02-26 12:35:45 +0200

    Update MainForm in both WinForms samples (bigger button and a hint)

--- WinForms/EqDemoWinFormsNet4/MainForm.Designer.cs
+++ WinForms/EqDemoWinFormsNet4/MainForm.Designer.cs
@@ -30,23 +30,36 @@ namespace EqDemo
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(499, 256);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(143, 39);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(197, 86);
+            this.button1.Size = new System.Drawing.Size(368, 130);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Open EasyQuery";
+            this.button1.Text = "Open EasyQuery Form";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(140, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 61);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "(we placed all EasyQuery functionality to a separate form to make it easier to co" +
+    "py it to your own project)\r\n\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1227, 689);
+            this.ClientSize = new System.Drawing.Size(634, 301);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -58,5 +71,6 @@ namespace EqDemo
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- WinForms/EqDemoWinFormsNet5/MainForm.Designer.cs
+++ WinForms/EqDemoWinFormsNet5/MainForm.Designer.cs
@@ -30,24 +30,39 @@ namespace EqDemo
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(499, 256);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(160, 40);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(197, 86);
+            this.button1.Size = new System.Drawing.Size(363, 130);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Open EasyQuery";
+            this.button1.Text = "Open EasyQuery Form";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(160, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 73);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "(we placed all EasyQuery functionality to a separate form to make it easier to co" +
+    "py it to your own project)\r\n\r\n\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1227, 689);
+            this.ClientSize = new System.Drawing.Size(661, 337);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
@@ -58,5 +73,6 @@ namespace EqDemo
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
