2e74e88f689849d1ceaf52bc49afaa77476de464
Oleksandr Melnychenko <oleksandr.melnychenko@fts.com.ua>
2021-04-02 13:51:10 +0300

    Clear old css files

--- AspNetCore/Vue/AdvancedSearch/ClientApp/src/main.ts
+++ AspNetCore/Vue/AdvancedSearch/ClientApp/src/main.ts
@@ -5,8 +5,6 @@ import '@easyquery/ui/dist/assets/css/eq-icons-default.css';
 import '@easyquery/ui/dist/assets/css/easyquery-mobile.css';
 import '@easyquery/ui/dist/assets/css/eqview.css';
 import '@easyquery/ui/dist/assets/css/easychart.css';
-import '@easyquery/ui/dist/assets/css/eq-grid.css';
-import '@easyquery/ui/dist/assets/css/eq-grid-mobile.css';
 import '@easyquery/ui/dist/assets/css/easyfacets.css';
 import '@easydata/ui/dist/assets/css/easy-grid.css';
 import '@easydata/ui/dist/assets/css/easy-dialog.css';
