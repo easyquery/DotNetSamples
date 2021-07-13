536fc6e2df118e9794cb0042187c2203180a4b33
Sergiy Korzh <sergey.korzh@live.com>
2021-07-13 23:40:40 +0300

    Angular sample for .NET Core 2.1

---
+++ AspNetCore/Angular/AdvancedSearch2.1/ClientApp/src/app/easyquery/easyquery.component.ts
@@ -0,0 +1,85 @@
+import { Component, AfterViewInit} from '@angular/core';
+
+import { EqContext } from '@easyquery/core';
+import { EqViewOptions, AdvancedSearchView } from '@easyquery/ui';
+
+import '@easyquery/enterprise';
+
+@Component({
+    selector: 'easyquery',
+    templateUrl: './easyquery.component.html'
+})
+export class EasyQueryComponent implements AfterViewInit {
+
+    private QUERY_KEY = 'easyquerycomponent-query';
+
+    private context: EqContext;
+
+    private view: AdvancedSearchView;
+
+    constructor() {
+      
+    }
+   
+
+    ngAfterViewInit() {
+
+      //here we define some options for AdvancedSearchView
+      const viewOptions: EqViewOptions = {
+        enableExport: true,
+        serverExporters: ['pdf', 'excel', 'csv'],
+        loadModelOnStart: true,
+        loadQueryOnStart: false,
+        handlers: {
+          onError: (_, error) => {
+            console.error(error.sourceError);
+          }
+        },
+        result: {
+          showChart: true
+        }
+      }
+
+      this.view = new AdvancedSearchView();
+      this.context = this.view.getContext();
+
+      this.context
+        .useEndpoint('/api/easyquery')
+        .useEnterprise(() => {
+          this.view.init(viewOptions);
+        });
+
+      this.context.addEventListener('ready', () => {
+        const query = this.context.getQuery();
+
+        query.addChangedCallback(() => {
+          const data = JSON.stringify({
+            modified: query.isModified(),
+            query: query.toJSONData()
+          });
+          localStorage.setItem(this.QUERY_KEY, data);
+        });
+
+        //add load query from local storage
+        this.loadQueryFromLocalStorage();
+      });
+     }  
+
+    private loadQueryFromLocalStorage() {
+      const dataJson = localStorage.getItem(this.QUERY_KEY);
+      if (dataJson) {
+          const data = JSON.parse(dataJson);
+          const query = this.context.getQuery();
+          query.loadFromDataOrJson(data.query);
+          if (data.modified) {
+            query.fireChangedEvent();
+          }
+          else {
+            this.view.getContext().refreshWidgets();
+            this.view.syncQuery();
+          }
+          
+          setTimeout(() => this.view.fetchData(), 100);
+        }
+    };    
+}
