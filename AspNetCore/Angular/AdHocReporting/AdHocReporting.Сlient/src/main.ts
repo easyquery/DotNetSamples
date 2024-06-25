e2d12f46cee00f3c42b274e5ee4067b5ffc7e52b
Serhii Pimenov <serhii@pimenov.com.ua>
2024-06-25 21:55:08 +0300

    added fix for dark-side

--- AspNetCore/Angular/AdHocReporting/AdHocReporting.Сlient/src/main.ts
+++ AspNetCore/Angular/AdHocReporting/AdHocReporting.Сlient/src/main.ts
@@ -4,9 +4,6 @@ import { AppComponent } from './app/app.component';
 import {ReportView, ReportViewOptions} from "@easyquery/ui";
 import '@easyquery/enterprise';
 import '@easyquery/ui/dist/assets/css/easyquery.ui.css'
-import '@easydata/ui/dist/assets/css/easy-grid.css'
-import '@easydata/ui/dist/assets/css/easy-dialog.css'
-import '@easydata/ui/dist/assets/css/easy-forms.css'
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
